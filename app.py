af076da51546652ef2d92044dee6de499d58ec03
pescadero29 <zurielpescadero123@gmail.com>
2025-11-25 12:48:01 +0800

    nov 25,2025

--- app.py
+++ app.py
@@ -452,4 +452,4 @@ if __name__ == '__main__':
     # Ensure DB exists
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
